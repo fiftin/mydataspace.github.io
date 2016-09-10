373c7ac55cd66febb29f788a8311da5126a7e975
Denis Gukov <fiftin@outlook.com>
2016-09-10 21:06:20 +0600

    chore: gulp ui

--- js/dist/ui.js
+++ js/dist/ui.js
@@ -1154,7 +1154,7 @@ UI = {
   pages: new Pages(),
 
   isViewOnly: function() {
-    return window.location.hash != null && window.location.hash !== '#';
+    return window.location.hash != null && window.location.hash !== '' && window.location.hash !== '#';
   },
 
   getViewOnlyRoot: function() {
