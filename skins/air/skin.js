f34370c02ce1bfbcb72776e5c79294752c46f56f
Denis Gukov <fiftin@outlook.com>
2016-09-15 02:03:02 +0600

    feat: skins

---
+++ skins/air/skin.js
@@ -0,0 +1,3 @@
+//[Skin Customization]
+webix.skin.air.barHeight=34;webix.skin.air.tabbarHeight=36;webix.skin.air.rowHeight=34;webix.skin.air.listItemHeight=28;webix.skin.air.inputHeight=34;webix.skin.air.layoutMargin.wide=4;webix.skin.air.layoutMargin.space=10;webix.skin.air.layoutPadding.space=10;
+ webix.skin.set('air')
