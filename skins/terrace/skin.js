298f2573b20afe715b27d285de33d9ee9e07c452
Denis Gukov <fiftin@outlook.com>
2016-09-15 02:23:22 +0600

    feat: add skins

---
+++ skins/terrace/skin.js
@@ -0,0 +1,3 @@
+//[Skin Customization]
+webix.skin.terrace.barHeight=37;webix.skin.terrace.tabbarHeight=39;webix.skin.terrace.rowHeight=38;webix.skin.terrace.listItemHeight=28;webix.skin.terrace.inputHeight=30;webix.skin.terrace.layoutMargin.wide=20;webix.skin.terrace.layoutMargin.space=20;webix.skin.terrace.layoutPadding.space=20;
+ webix.skin.set('terrace')
