d57b623636fb1a8e897b46318a292624b567bb8d
Denis Gukov <denis.gukov@maginfo.com>
2016-11-15 16:50:27 +0500

    refactor: delete unused skins

--- skins/clouds/skin.js
+++
@@ -1,3 +0,0 @@
-//[Skin Customization]
-webix.skin.clouds.barHeight=36;webix.skin.clouds.tabbarHeight=46;webix.skin.clouds.rowHeight=34;webix.skin.clouds.listItemHeight=32;webix.skin.clouds.inputHeight=30;webix.skin.clouds.layoutMargin.wide=4;webix.skin.clouds.layoutMargin.space=10;webix.skin.clouds.layoutPadding.space=10;
- webix.skin.set('clouds')
