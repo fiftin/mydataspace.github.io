49da205c8529cff436b0571c73b908926da1bb68
Denis Gukov <denguk@gmail.com>
2017-01-26 00:35:48 +0500

    feat(tree): search by url

--- js/router.js
+++ js/router.js
@@ -4,16 +4,26 @@ var Router = {
            window.location.hash === '' ||
            window.location.hash === '#';
   },
+  /**
+   * Route links to single root.
+   */
   isRoot: function() {
-    return window.location.hash.indexOf('*') < 0;
+    return !Router.isEmpty() && window.location.hash.indexOf('*') < 0;
   },
   getRoot: function() {
     return window.location.hash.substring(1);
   },
   isSearch: function() {
-    return !Router.isRoot();
+    return !Router.isEmpty() && !Router.isRoot();
   },
   getSearch: function() {
-    return window.location.hash.substring(1).replace(/\*/g, '');
+    var s = window.location.hash.substring(1);
+    if (Router.isMe()) {
+      s = s.substring(3);
+    }
+    return s.replace(/\*/g, '');
+  },
+  isMe: function() {
+    return window.location.hash.indexOf('#me:') === 0;
   }
 };
