65a8fd0d37f3659b1380fe87f4655bad0d4e2131
Denis Gukov <denguk@gmail.com>
2017-02-22 18:44:37 +0500

    fix: no desc or readme to center|

--- js/entity-form.js
+++ js/entity-form.js
@@ -225,7 +225,7 @@ EntityForm.prototype.setRootView = function(data) {
 
     if (MDSCommon.isBlank(description)) {
       if (MDSCommon.isBlank(readme)) {
-        document.getElementById('view__description').innerHTML = '<i>No description or README provided.</i>';
+        document.getElementById('view__description').innerHTML = '<center><i>' + STRINGS.NO_README + '</i></center>';
       } else {
         document.getElementById('view__description').style.display = 'none';
       }
