6f435e19db6a24415e4ca37c2fa331fa4f8ff6b7
Denis Gukov <denguk@gmail.com>
2017-05-10 19:51:20 +0500

    chore: gulp api

--- js/dist/mydataspace.js
+++ js/dist/mydataspace.js
@@ -17,7 +17,10 @@ if (global.localStorage == null) {
 
 'use strict';
 
+
 var MDSCommon = {
+  BASE32: '0123456789bcdefghjkmnpqrstuvwxyz',
+
   primitiveTypes: [
     'number',
     'string',
@@ -25,6 +28,80 @@ var MDSCommon = {
     'undefined',
     'function'
   ],
+
+  refine_interval: function(interval, cd, mask) {
+    if (cd&mask)
+      interval[0] = (interval[0] + interval[1])/2;
+    else
+      interval[1] = (interval[0] + interval[1])/2;
+  },
+
+  calculateAdjacent: function(srcHash, dir) {
+    var NEIGHBORS = { right  : { even :  "bc01fg45238967deuvhjyznpkmstqrwx" },
+      left   : { even :  "238967debc01fg45kmstqrwxuvhjyznp" },
+      top    : { even :  "p0r21436x8zb9dcf5h7kjnmqesgutwvy" },
+      bottom : { even :  "14365h7k9dcfesgujnmqp0r2twvyx8zb" } };
+
+    NEIGHBORS.bottom.odd = NEIGHBORS.left.even;
+    NEIGHBORS.top.odd = NEIGHBORS.right.even;
+    NEIGHBORS.left.odd = NEIGHBORS.bottom.even;
+    NEIGHBORS.right.odd = NEIGHBORS.top.even;
+
+
+    var BORDERS   = { right  : { even : "bcfguvyz" },
+      left   : { even : "0145hjnp" },
+      top    : { even : "prxz" },
+      bottom : { even : "028b" } };
+
+    BORDERS.bottom.odd = BORDERS.left.even;
+    BORDERS.top.odd = BORDERS.right.even;
+    BORDERS.left.odd = BORDERS.bottom.even;
+    BORDERS.right.odd = BORDERS.top.even;
+
+
+    srcHash = srcHash.toLowerCase();
+    var lastChr = srcHash.charAt(srcHash.length-1);
+    var type = (srcHash.length % 2) ? 'odd' : 'even';
+    var base = srcHash.substring(0,srcHash.length-1);
+    if (BORDERS[dir][type].indexOf(lastChr) !== -1) {
+      base = MDSCommon.calculateAdjacent(base, dir);
+    }
+    return base + MDSCommon.BASE32[NEIGHBORS[dir][type].indexOf(lastChr)];
+  },
+
+  decodeGeoHash: function(geohash) {
+    var BITS = [16, 8, 4, 2, 1];
+
+    var is_even = 1;
+    var lat = []; var lon = [];
+    lat[0] = -90.0;  lat[1] = 90.0;
+    lon[0] = -180.0; lon[1] = 180.0;
+    var lat_err = 90.0;
+    var lon_err = 180.0;
+    var i;
+    var cd;
+    var mask;
+
+    for (i=0; i<geohash.length; i++) {
+      c = geohash[i];
+      cd = MDSCommon.BASE32.indexOf(c);
+      for (j=0; j<5; j++) {
+        mask = BITS[j];
+        if (is_even) {
+          lon_err /= 2;
+          MDSCommon.refine_interval(lon, cd, mask);
+        } else {
+          lat_err /= 2;
+          MDSCommon.refine_interval(lat, cd, mask);
+        }
+        is_even = !is_even;
+      }
+    }
+    lat[2] = (lat[0] + lat[1])/2;
+    lon[2] = (lon[0] + lon[1])/2;
+
+    return { latitude: lat, longitude: lon};
+  },
 
   guid: function() {
     function s4() {
