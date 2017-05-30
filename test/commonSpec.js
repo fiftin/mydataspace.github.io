8492a375cea632f283bbbd21ed202bc11d671d47
Denis Gukov <denguk@gmail.com>
2017-05-30 11:21:25 +0500

    fix(import): regex result

--- test/commonSpec.js
+++ test/commonSpec.js
@@ -1,5 +1,51 @@
 describe('MDSCommon', function() {
+
+
+
+
+
   describe('#diff', function() {
+    it('', function() {
+      const oldData = { root: 'test567',
+        path: 'processes/02863847-0fe9-c517-1eb8-cec0100b4d87',
+        othersCan: 'view_children',
+        createdAt: '2017-05-30T06:07:47.000Z',
+        updatedAt: '2017-05-30T06:07:47.000Z',
+        fields: 
+         { status: 'unaffected',
+           type: 'importOverOpenRefine',
+           targetPath: 'data',
+           id: '?id=e2d1f18b-f2bb-44a6-b46b-12637dbece66',
+           state: 'paused',
+           targetState: 'progress' },
+        numberOfChildren: 0,
+        maxNumberOfChildren: 1000000,
+        mine: true };
+
+      const newData = { root: 'test567',
+        path: 'processes/02863847-0fe9-c517-1eb8-cec0100b4d87',
+        othersCan: 'view_children',
+        fields: 
+         { id: '?id=e2d1f18b-f2bb-44a6-b46b-12637dbece66',
+           state: 'progress',
+           status: 'unaffected',
+           targetPath: 'data',
+           targetState: 'progress',
+           type: 'importOverOpenRefine' },
+        numberOfChildren: 0,
+        createdAt: '2017-05-30T06:07:47.000Z',
+        updatedAt: '2017-05-30T06:07:47.000Z',
+        isSystem: false,
+        maxNumberOfChildren: 1000000,
+        mine: true,
+        requestId: 10001 };
+
+      const diff = MDSCommon.diff(newData, oldData);
+      console.log(diff);
+
+      expect(diff.fields.state).to.eq('progress');
+
+    });
     it('valid difference of simple object', function() {
       var res = MDSCommon.diff({
         address: 'Lenin st',
