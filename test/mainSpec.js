52e5ed0845d8eeb8510aa2c0b3639ff8cf9057db
Denis Gukov <denguk@gmail.com>
2017-11-01 02:32:37 +0500

    fix(root page): data passed to look snippet's template

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,7 +1,7 @@
 describe('main', function() {
   describe('#getHtmlFromTemplate', function() {
     it('Should return valid HTML', function() {
-      var res = getHtmlFromTemplate('<div>{{ name }}</div>\n' +
+      var res = getHtmlFromTemplate('<div>{{name }}</div>\n' +
         '<div>{{ last_name }}</div>', { name: 'Denis', last_name: 'Gukov' });
       expect(res).eq('<div>Denis</div>\n<div>Gukov</div>');
     });
