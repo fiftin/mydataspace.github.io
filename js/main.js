9eb34c495124e1bf8282fb58a121e2908465ab05
Denis <denguk@gmail.com>
2017-11-14 10:25:51 +0500

    fix(list view): ymap zoom

--- js/main.js
+++ js/main.js
@@ -302,7 +302,7 @@ Handlescripts.registerHelper('ymapsAddr', function(options) {
     '    var point = data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(\' \').map(function(p){ return parseFloat(p); }).reverse();\n' +
     '    var myMap = new ymaps.Map(\'ymap\', {\n' +
     '            center: point,\n' +
-    '            zoom: 13\n' +
+    '            zoom: 14\n' +
     '        });\n' +
     '    myMap.geoObjects.add(new ymaps.Placemark(point));\n' +
     '});' +
