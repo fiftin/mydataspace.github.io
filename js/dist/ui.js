18569da77f6dcc1a9698dde560fca0e666490380
Denis <denguk@gmail.com>
2019-02-28 19:17:54 +0500

    ...

--- js/dist/ui.js
+++ js/dist/ui.js
@@ -5710,15 +5710,15 @@ UILayout.entityContextMenu = {
         icon: 'edit'
       });
 
-      menuItems.push({
-        id: 'copy_entity',
-        value: STRINGS.context_menu.copy_entity
-      });
-
-      menuItems.push({
-        id: 'delete_entity',
-        value: STRINGS.context_menu.delete_root
-      });
+      // menuItems.push({
+      //   id: 'copy_entity',
+      //   value: STRINGS.context_menu.copy_entity
+      // });
+      //
+      // menuItems.push({
+      //   id: 'delete_entity',
+      //   value: STRINGS.context_menu.delete_root
+      // });
 
       this.data.add({ $template: 'Separator' });
 
