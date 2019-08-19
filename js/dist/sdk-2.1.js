578d657ef7100399178eac72d6930eab97f26ddf
Denis Gukov <denguk@gmail.com>
2019-08-19 21:56:59 +0500

    ...

--- js/dist/sdk-2.1.js
+++ js/dist/sdk-2.1.js
@@ -7626,7 +7626,7 @@ var MDSCommon = {
   hash: function(str) {
     var hash = 0, i, chr;
     if (str.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
+    for (i = 0; i < str.length; i++) {
       chr   = str.charCodeAt(i);
       hash  = ((hash << 5) - hash) + chr;
       hash |= 0; // Convert to 32bit integer
